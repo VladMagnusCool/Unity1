ffbb2c85aa68a4d0d31a7f05224149b99d86d056
VladMagnusCool <39080419+VladMagnusCool@users.noreply.github.com>
2018-05-08 10:15:31 +0300

    Add files via upload

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    private Rigidbody rb;
+	// Use this for initialization
+	void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float v = Input.GetAxis("Vertical");
+        float h = Input.GetAxis("Horizontal");
+
+        Vector3 dir = new Vector3(h, 0, v);
+        rb.AddForce(dir);
+    }
+}
